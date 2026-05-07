c69505ff551f30524ea48f5425370a70c31934e5
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2026-05-07 07:50:38 -0300

    Feat: Adiciona projeto Spring

---
+++ src/main/java/com/example/smartcollector/SmartcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartcollector/SmartcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
